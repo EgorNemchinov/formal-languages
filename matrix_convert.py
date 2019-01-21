9c2e949c1dd45829767ce48f772a1a3813385207
Egor Nemchinov <immortalsuperturtle@gmail.com>
2019-01-21 10:04:57 +0300

    Shared memory operations; compress bits into uint8 & uint32

--- matrix_convert.py
+++ matrix_convert.py
@@ -1,7 +1,7 @@
 import numpy as np
 import numba
 from logger import log
-from matrix_operations import BOOL_TYPE_VALUES
+from matrix_operations import TPB, BOOL_TYPE_VALUES, BYTE_TYPE_VALUES, INT_TYPE_VALUES, shared_memory
 
 
 def to_type(matrices, type):
@@ -10,6 +10,19 @@ def to_type(matrices, type):
         if matrix.dtype in BOOL_TYPE_VALUES:
             log('Matrices already bool, returning them..')
             return matrices
+        elif type in BYTE_TYPE_VALUES:
+            if shared_memory:
+                size = 8 * TPB[0]
+                matrix = np.pad(matrix, [(0, (size - matrix.shape[0]) % size), (0, (size - matrix.shape[1]) % size)], 'constant')
+            matrix = np.packbits(matrix, axis=-1)
+            matrices[key] = matrix
+        elif type in INT_TYPE_VALUES:
+            if shared_memory:
+                size = 32 * TPB[0]
+                matrix = np.pad(matrix, [(0, (size - matrix.shape[0]) % size), (0, (size-matrix.shape[1]) % size)], 'constant')
+            matrix = np.pad(matrix, [(0, 0), (0, (32 - matrix.shape[1] % 32) % 32)], 'constant').astype(np.uint32)
+            packed_matrix = sum(matrix[:, i::32] << (31 - i) for i in range(32))
+            matrices[key] = packed_matrix
         else:
             raise ValueError('Matrix type {} is not supported'.format(type))
     return matrices
--- matrix_operations.py
+++ matrix_operations.py
@@ -9,6 +9,8 @@ TPB = (16, 16)
 sB_size8 = TPB[0] * 8
 sB_size32 = TPB[1] * 32
 BOOL_TYPE_VALUES = ['bool', np.bool, numba.boolean]
+BYTE_TYPE_VALUES = [np.uint8, numba.uint8, 'uint8', 'byte']
+INT_TYPE_VALUES = [np.uint32, numba.uint32, 'uint32', 'int']
 
 
 def update_matrix(matrices, head, body):
@@ -34,20 +36,20 @@ def update_matrix_gpu(matrices, head, body):
     body_first_mat, body_second_mat = matrices[body[0]], matrices[body[1]]
     is_changed = cuda.device_array((1,), dtype=bool)
 
-    blockspergrid = (int(math.ceil(body_first_mat.shape[i] / TPB[i])) for i in (0, 1))
+    blockspergrid = tuple(int(math.ceil(body_first_mat.shape[i] / TPB[i])) for i in (0, 1))
 
     if str(head_mat.dtype) == 'bool':
         matmul[blockspergrid, TPB](body_first_mat, body_second_mat, head_mat, is_changed)
         return is_changed[0]
     elif str(head_mat.dtype) == 'uint8':
         if shared_memory:
-            raise NotImplementedError('Shared memory on type {} on GPU is not supported'.format(head_mat.dtype))
+            matmul_packed8_shared[blockspergrid, TPB](body_first_mat, body_second_mat, head_mat, is_changed)
         else:
             matmul_packed8[blockspergrid, TPB](body_first_mat, body_second_mat, head_mat, is_changed)
         return is_changed[0]
     elif str(head_mat.dtype) == 'uint32':
         if shared_memory:
-            raise NotImplementedError('Shared memory on type {} on GPU is not supported'.format(head_mat.dtype))
+            matmul_packed32_shared[blockspergrid, TPB](body_first_mat, body_second_mat, head_mat, is_changed)
         else:
             matmul_packed32[blockspergrid, TPB](body_first_mat, body_second_mat, head_mat, is_changed)
         return is_changed[0]
@@ -108,5 +110,89 @@ def matmul_packed32(A, B, C, is_changed):
         C[row, col] = new_value
         is_changed[0] = True
 
+@cuda.jit
+def matmul_packed8_shared(A, B, C, is_changed):
+    row, col = cuda.grid(2)
+    tx = cuda.threadIdx.x
+    ty = cuda.threadIdx.y
 
+    size = x_interval = 8
+
+    sA = cuda.shared.array(shape=(TPB[0], TPB[1]), dtype=numba.uint8)
+    sB = cuda.shared.array(shape=(sB_size8, TPB[1]), dtype=numba.uint8)
+
+    if row >= C.shape[0] or col >= C.shape[1]:
+        return
+    value = 0
+    for step in range(math.ceil(A.shape[1] / TPB[1])):
+        if step * TPB[0] + ty < A.shape[1]:
+            sA[tx, ty] = A[row, step * TPB[0] + ty]
+        else:
+            sA[tx, ty] = 0
+
+        for l in range(x_interval):
+            if tx * x_interval + step * sB_size8 + l < B.shape[0]:
+                sB[tx * x_interval + l, ty] = B[tx * x_interval + step * sB_size8 + l, col]
+            else:
+                sB[tx, ty] = 0
+
+        cuda.syncthreads()
+
+        for k in range(TPB[1]):
+            cur_value_A = sA[tx, k]
+            for j in range(size - 1, -1, -1):
+                if cur_value_A & 1:
+                    value |= (sB[k * size + j, ty])
+                cur_value_A >>= 1
+
+        cuda.syncthreads()
+
+    old_value = C[row, col]
+    new_value = old_value | value
+    if new_value != old_value:
+        C[row, col] = new_value
+        is_changed[0] = True
+
+@cuda.jit
+def matmul_packed32_shared(A, B, C, is_changed):
+    row, col = cuda.grid(2)
+    tx = cuda.threadIdx.x
+    ty = cuda.threadIdx.y
+
+    size = x_interval = 32
+
+    sA = cuda.shared.array(shape=(TPB[0], TPB[1]), dtype=numba.uint32)
+    sB = cuda.shared.array(shape=(sB_size32, TPB[1]), dtype=numba.uint32)
+
+    if row >= C.shape[0] or col >= C.shape[1]:
+        return
+    value = 0
+    for step in range(math.ceil(A.shape[1] / TPB[1])):
+        if step * TPB[0] + ty < A.shape[1]:
+            sA[tx, ty] = A[row, step * TPB[0] + ty]
+        else:
+            sA[tx, ty] = 0
+
+        for l in range(x_interval):
+            if tx * x_interval + step * sB_size32 + l < B.shape[0]:
+                sB[tx * x_interval + l, ty] = B[tx * x_interval + step * sB_size32 + l, col]
+            else:
+                sB[tx, ty] = 0
+
+        cuda.syncthreads()
+
+        for k in range(TPB[1]):
+            cur_value_A = sA[tx, k]
+            for j in range(size - 1, -1, -1):
+                if cur_value_A & 1:
+                    value |= (sB[k * size + j, ty])
+                cur_value_A >>= 1
+
+        cuda.syncthreads()
+
+    old_value = C[row, col]
+    new_value = old_value | value
+    if new_value != old_value:
+        C[row, col] = new_value
+        is_changed[0] = True
 
